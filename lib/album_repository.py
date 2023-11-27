8cb013ac67bc64f3e1311449e35ecfc1bba6689d
Ruby <rubyseresin@gmail.com>
2023-11-27 16:02:34 +0000

    created album classes

---
+++ lib/album_repository.py
@@ -0,0 +1,16 @@
+from lib.album import Album
+
+class AlbumRepository:
+
+    # We initialise with a database connection
+    def __init__(self, connection):
+        self._connection = connection
+
+    # Retrieve all artists
+    def all(self):
+        rows = self._connection.execute('SELECT * from albums')
+        albums = []
+        for row in rows:
+            item = Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+            albums.append(item)
+        return albums
